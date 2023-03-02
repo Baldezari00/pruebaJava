98e82782df94f64057a1faac0e625de7c783b366
Baldezari00 <110638177+Baldezari00@users.noreply.github.com>
2023-03-02 20:25:41 -0300

    segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int holi = 5;
+        holi = 1;
+        System.out.println(holi);
     }
-
     }
+    /*para volver para atras en una version de inteligi apretar en "git" y show git log y ahi te aparecen todos los commits  */
